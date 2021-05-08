c6a4a46bfa0e94c15f3eb94930f2087e5b8b315d
Rkayji <mrrkayj86@gmail.com>
2021-05-08 21:37:24 +0530

    mvc framework created

---
+++ php/php_framwork/app/require.php
@@ -0,0 +1,9 @@
+<?php
+// require the libraries from libraries folder
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
+
+require_once 'config/config.php';
+// Initiate the core class
+$init = new Core();
--- php/php_framwork/test.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "this is just for testing";
